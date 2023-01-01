3922f0ba6369ddea32530cb560eb1cfeaaad39ee
apdy07 <119981586+apdy07@users.noreply.github.com>
2023-01-01 15:07:42 +0530

    Create H. Two numbers.cpp

---
+++ Beginner CP/Sheet #1 (Data type - Conditions)/H. Two numbers.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float a,b;
+    cin>>a>>b;
+    float m = a/b;
+    cout<<"floor "<<a<<" / "<<b<<" = "<<floor(m)<<endl;
+    cout<<"ceil "<<a<<" / "<<b<<" = "<<ceil(m)<<endl;
+    cout<<"round "<<a<<" / "<<b<<" = "<<round(m)<<endl;
+
+}
+
+
+

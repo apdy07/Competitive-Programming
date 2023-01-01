34eb0825487d84fc6bb7271846d79a44bc72eeac
apdy07 <119981586+apdy07@users.noreply.github.com>
2023-01-01 16:17:02 +0530

    Create N. Char.cpp

---
+++ Beginner CP/Sheet #1 (Data type - Conditions)/N. Char.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char ch;
+    cin>>ch;
+    if(ch>97){
+        ch = ch-32;
+    }else{
+        ch = ch+32;
+    }
+    cout<<ch;
+}

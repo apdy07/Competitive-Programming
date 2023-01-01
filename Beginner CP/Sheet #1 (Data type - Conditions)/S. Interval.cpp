49b3716a90be8f813733ee0c3135fca41cbe3acf
apdy07 <119981586+apdy07@users.noreply.github.com>
2023-01-01 17:57:18 +0530

    Create S. Interval.cpp

---
+++ Beginner CP/Sheet #1 (Data type - Conditions)/S. Interval.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float x;
+    cin>>x;
+    // [0,25], (25,50], (50,75], (75,100]
+    if(x>=0 && x<=25){
+        cout<<"Interval [0,25]";
+    }else if(x>25 && x<=50){
+        cout<<"Interval (25,50]";
+    }else if(x>50 && x<=75){
+        cout<<"Interval (50,75]";
+    }else if(x>75 && x<=100){
+        cout<<"Interval (75,100]";
+    }else{
+        cout<<"Out of Intervals";
+    }
+}

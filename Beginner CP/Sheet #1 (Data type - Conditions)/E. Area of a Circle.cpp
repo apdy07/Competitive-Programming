f0dfbb8f8f377932b3a99ffc30684150f371b9a0
apdy07 <119981586+apdy07@users.noreply.github.com>
2023-01-01 14:52:49 +0530

    Create E. Area of a Circle.cpp

---
+++ Beginner CP/Sheet #1 (Data type - Conditions)/E. Area of a Circle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    double pi = 3.141592653;
+    float r;
+    cin>>r;
+    cout<<setprecision(11);
+    cout<<pi*r*r;
+
+}

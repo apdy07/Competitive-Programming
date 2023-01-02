5096a2d8899473fa057c11de17a3a85cb30dee39
apdy07 <119981586+apdy07@users.noreply.github.com>
2023-01-02 10:29:41 +0530

    Create E. Max.cpp

---
+++ Beginner CP/Sheet #2 (Loops)/E. Max.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    int max = INT_MIN;
+    cin>>t;
+    while(t--){
+        int a;
+        cin>>a;
+        if(a>max){
+            max = a;
+        }
+
+    }
+    cout<<max;
+}

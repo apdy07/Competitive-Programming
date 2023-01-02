46cf715a0b372d978e3abe960deb1937e5e42070
apdy07 <119981586+apdy07@users.noreply.github.com>
2023-01-02 08:28:28 +0530

    Create Basic Data Types.cpp

---
+++ HackerRank C++/Basic Data Types.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c;
+    cin>>d>>e;
+    cout<<a<<endl<<b<<endl<<c<<endl;
+    cout<<fixed<<setprecision(3);
+    cout<<d<<endl;
+    cout<<fixed<<setprecision(9);
+    cout<<e<<endl;
+    
+    
+    return 0;
+}

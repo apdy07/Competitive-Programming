fe150a2b9474447e13610f6c07d4c1934e7b92a0
apdy07 <119981586+apdy07@users.noreply.github.com>
2023-01-03 21:48:29 +0530

    Create I. Welcome for you with Conditions.cpp

---
+++ Beginner CP/Sheet #1 (Data type - Conditions)/I. Welcome for you with Conditions.cpp
@@ -0,0 +1,13 @@
+//welcome for you with conditions
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    if(a>=b){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+}

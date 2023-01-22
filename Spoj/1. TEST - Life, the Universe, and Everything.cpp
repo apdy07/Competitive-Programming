38910af8d1db92c44f5401f6691bd45d11faa351
apdy07 <119981586+apdy07@users.noreply.github.com>
2023-01-23 05:20:04 +0530

    Create 1. TEST - Life, the Universe, and Everything.cpp

---
+++ Spoj/1. TEST - Life, the Universe, and Everything.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    while(1){
+        int x;
+        cin>>x;
+        if(x==42){
+            break;
+        }
+        cout<<x<<endl;
+    }
+}

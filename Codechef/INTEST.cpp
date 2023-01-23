2b544781e67aa0061604a4eec6e88a39f426448d
apdy07 <119981586+apdy07@users.noreply.github.com>
2023-01-23 07:00:45 +0530

    Create  INTEST.cpp

---
+++ Codechef/INTEST.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,k,count=0;
+    cin>>n>>k;
+    int A[n];
+    for(int i=0;i<n;i++){
+        cin>>A[i];
+    }
+    for(int i=0;i<n;i++){
+        if(A[i]%k == 0){
+            count++;
+        }
+    
+}
+cout<<count;
+}
